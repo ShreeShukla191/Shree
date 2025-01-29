0f525da1ca3910f25da35af6da96d76befabe1cb
ShreeShukla191 <shreeshukla56@gmail.com>
2025-01-29 12:26:56 +0530

    Add files via upload

---
+++ abcpattern.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int n=6,i,j;
+	for(i=0;i<n;i++)
+	{
+		for( j=0;j<n-1;j++){
+			printf("%c",);
+		}
+	}
+}
---
+++ pattern 1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+
+   n=5;
+  int i,j;
+   for(i=0;i<n;i++)
+   {
+	   for( j=0;j<i+1;j++)
+	   {
+	   	printf("* ");
+	   }
+	   printf("\n");
+	}
+	   returintn 0;
+   }
